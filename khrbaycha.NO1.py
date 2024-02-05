95a4a0af9c73edb1160f7d99835327c1a00decb4
OUssiUDBmE <158707383+OUssiUDBmE@users.noreply.github.com>
2024-02-05 14:45:31 +0100

    Add files via upload

---
+++ khrbaycha.NO1.py
@@ -0,0 +1,18 @@
+pp=6
+x=input('insert number-->')
+l=int(x)
+if l>10:
+     try:
+        print('this is try command')
+     except:
+      k=1
+     l=int(pp)
+     print('this is exept commmand')
+if l < 10 :
+   try:
+      f=33
+      print (f*l)
+   except:
+      print(1+f)
+      
+   
